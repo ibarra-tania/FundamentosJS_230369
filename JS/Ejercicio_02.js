6815526fd4897db49499b0c97810e5084a2dbbb9
Tania Ibarra Salgado. <230369@utxicotepec.edu.mx>
2024-09-22 16:38:31 -0600

    Practica 4
    Se agrego el ultimo tipo de dato FUNCIÓN

--- JS/Ejercicio_02.js
+++ JS/Ejercicio_02.js
@@ -252,5 +252,14 @@ console.log(
 
 
 // 8. FUNCTION (funciones)
-    
+    //8. FUNCTION (Funciones)
+console.warn("--- Tipo de Dato FUNCTION (Función)")
+//Declaramos una función que nos permita recibir un parámetro, en este caso, el nombre de la persona a saludar, y le enviamos un saludo. Esta función la asignamos a una constante.
+const saludar= function(nombre){return Hola, ${nombre}}
+
+//Invocamos a la función declarada
+console.log(saludar('Tania'));
+
+//Y que tipo de dato tiene esta constante.
+console.log(`El tipo de dato de la constante saludar es: ${typeof(saludar)}`);
 
--- README.md
+++ README.md
@@ -16,7 +16,7 @@ el objetivo de confirmar los conocimientos básicos de lógica de programación.
  |1.|Metodología de Evaluación|1|✅Finalizada|
  |2.| Inicialización del Proyecto de Fundamentos de JS|8|✅Finalizada|
  |3.| Ejercicio 01: Declaración de Variables en JS|--|✅Finalizada|
- |4.|Ejercicio 02: Declaración de Variables (Undefined/Boolean/Number/String/BigInt/Symbol/Null/Function)|16|Activa|
+ |4.| Ejercicio 02: Declaración de Variables (Undefined/Boolean/Number/String/BigInt/Symbol/Null/Function)|16|Activa|
 
 
 ### Autor
