4756a0500621e37242e4b065d064ebc740fb2016
Tania Ibarra Salgado. <230369@utxicotepec.edu.mx>
2024-09-29 16:57:29 -0600

    Practica 5
    Se agregaron modificaciones el Ejercicio_03.js

--- JS/Ejercicio_03.js
+++ JS/Ejercicio_03.js
@@ -71,3 +71,78 @@ if(Producto.Disponibilidad == 0)
 console.log(`Estatus: Agotado`)
 else 
 console.log(`Estatus: ${Producto_Stock} unidades disponibles.`);
+
+// Desestructuración de Objetos.
+
+console.log("%c3.- Destructuración de Objetos", style_console);
+
+let Producto2 =
+{   
+    Clave: 316,
+    Nombre : "Lentes para Sol", 
+    Marca: "Oakley",
+    Modelo: "QNTM Kato", 
+    Precio: 6829.00,
+    Disponibilidad: true,
+    Stock: 5,
+    SKU: "OO9481D-0356",
+    Imagen: "../assets/products/sunglasses/KatoRed.png",
+    Barcode:  888392491626,
+    Categorias: ["Deportes", "Lentes", "Hombre", "Accesorios"]
+}
+
+
+let Comprador =
+{
+    ID: 3216,
+    Nombre: "Marco",
+    Apellidos: "Ramírez Hernández",
+    Tipo: "Frecuente",
+    Correo: "mar.ram@gmail.com",
+    PaisOrigen: "México",
+    SaldoActual: 14000.00
+}
+
+let Pedido= {
+    ID:5816,
+    Producto_ID: 316, 
+    Comprador_ID: 3216,
+    Cantidad: 2, 
+    Estatus: "Carrito de Compra",
+    TipoPago: "Tarjeta de Crédito"
+}
+
+// En base  a los 3 objetos necesitamos calcular el costo de la compra y si le alcanza con sus saldo a favor
+
+let {Precio: Producto_Precio2 } = Producto2; 
+let {Cantidad:Pedido_Cantidad} = Pedido;
+let {SaldoActual: Cliente_SaldoActual} = Comprador;
+let Costo_Compra= Producto_Precio2 * Pedido_Cantidad;
+
+console.log(`El cliente ha agregado a su carrito de compras ${Pedido_Cantidad} undidades, con un costo total de: $${Costo_Compra}`);
+if(Costo_Compra< Cliente_SaldoActual)
+    console.log("El cliente tiene saldo suficiente.");
+
+// Actualizar el valor de los Objetos
+
+console.log("%c4.- Actualización de los valores de las propiedades de un Objeto", style_console);
+
+console.log(`El objeto actualmente tiene los siguientes valores`)
+console.log(JSON.stringify(Producto2, null, 2));
+// Convierte el objeto a una cadena para evitar problemas con la referencia
+console.log(`Por cuestiones de inflación el costo del producto ha  cambiado y debe ser actualizado... de $6,829.00 a $6,915.50`)
+// Para podificiar el valor de un objeto basta con igualar el nuevo valor de la proppiedad deseada
+Producto2.Precio=6915.50;
+console.log(`Los nuevos valores del Prodcuto son:  `)
+console.log(Producto2);
+
+// ¿Puedo cambiar no solo el valor , sino el tipo de dato de un Objeto en JavaScript?
+console.log(`------------------------------------------------------------`)
+console.log(`El objeto actualmente tiene los siguientes valores`)
+let tipoDisponibilidad = typeof(Producto2.Disponibilidad)
+console.log(`El tipo de dato de la disponibilidad es: ${tipoDisponibilidad}`)
+console.log(JSON.stringify(Producto2, null, 2));    // Disponiblidad Booleano
+Producto2.Disponibilidad="Sí";
+let nuevoTipoDisponibilidad = typeof(Producto2.Disponibilidad)
+console.log(Producto2);
+console.log(`El nuevo tipo de dato de la disponibilidad es: ${nuevoTipoDisponibilidad}`)
