e0302ce7d1dc4f8b364372c711b98ade1a6dfdc5
Tania Ibarra Salgado. <230369@utxicotepec.edu.mx>
2024-10-09 19:42:37 -0600

    Práctica 07
    Se incluyeron los puntos 2 y 3 en el Ejercicio_05.js

--- JS/Ejercicio_05.js
+++ JS/Ejercicio_05.js
@@ -41,16 +41,92 @@ else
 
 
 //que pasa si la validación tiene varias operaciones 
-const anio= fechaActual.getFullYear()
-let inicioPrimavera= new Date(anio, 2, 21);
-let inicioVerano= new Date(anio, 5, 21);
-let inicioOtonio= new Date(anio, 8, 23);
-let inicioInvierno= new Date(anio, 11, 21);
+const anio=fechaActual.getFullYear();
+let inicioPrimavera=new Date(anio, 2, 21);
+let inicioVerano=new Date(anio, 5, 21);
+let inicioOtonio=new Date(anio, 8, 23);
+let inicioInvierno=new Date(anio, 11, 21);
+let estacion;
+let horarioVerano=false;
 
-let horarioVerano= false;
-
-if(fechaActual>= inicioPrimavera || fechaActual< inicioVerano)
-{
-    console.log("Estamos en PRIMAVERA...")
-
+if(fechaActual>=inicioPrimavera && fechaActual<inicioVerano){
+    console.log(`Estamos en PRIMAVERA`);
+    console.log("Inicia la floración de muchas plantas.")
+    console.log("Los días son más largos y las noches más cortas.")
+    console.log("Muchos animales despiertan de la hibernación.")
+    estacion="Primavera";
+    horarioVerano=true;
+} else if(fechaActual>=inicioVerano&&fechaActual<inicioOtonio){
+    console.log("Estamos en VERANO");
+    console.log("En esta temporada abundan los días soleados y calurosos.")
+    console.log("En esta temporada el índice de turismo vacacional sube.")
+    console.log("Mucha gente busca realizar actividades al aire.")
+    estacion="Verano";
+    horarioVerano=true;
+}else if(fechaActual>=inicioOtonio&&fechaActual<inicioInvierno){
+    console.log("Estamos en OTOÑO")
+    console.log("Los árboles suelen cambiar de follaje.")
+    console.log("Se registrarán temperaturas más templadas.")
+    console.log("Los animales comienzan con la recolección de alimento y preparan sus espacios para la hibernación, incluso algunas aves migran.")
+    estacion="Otoño";
+    horarioVerano=false;
+} else {
+    console.log("Estamos en INVIERNO")
+    console.log("En esta temporada los días son más cortos y las noches más largas.")
+    console.log("En algunas regiones suele nevar.")
+    console.log("Dado las bajas temperaturas, se recomienda abrigarse.")
+    estacion="Invierno";
+    horarioVerano=false;
 }
+
+
+
+console.log("%c2.- Operador Ternario (validación?cumple:no_cumple)", style_console);
+//En JavaScript existe una operación simplificada que valida si una condición se cumple o no, y que hacer en cada caso.
+
+const edadPersona=18;
+const mayoriaEdadMX=18;
+const mayoriaEdadUS=21;
+let evaluarMayoriaEdad=(edad)=>
+    edad>=18?"Eres mayor de edad.":"No eres mayor de edad."
+
+console.log("Evaluando la mayoría de edad de una persona: ");
+console.log(evaluarMayoriaEdad(edadPersona));
+
+//Sin embargo tenemos que considerar que la mayoría de edad varia en cada país por cuestiones legales, por lo que debemos considerar un segundo parámetro en la evaluación.
+
+evaluarMayoriaEdad=(edad, pais)=>
+    (edad>=18 && pais=="MX")?`En ${pais} eres mayor de edad.`:`En ${pais} NO eres mayor de edad.`
+
+
+console.log("Evaluando la mayoría de edad de una persona en Estados Unidos")
+console.log(evaluarMayoriaEdad(edadPersona, "USA"))
+
+
+
+
+console.log("%c3.- SWITCH - CASE (Elección por valor definido)", style_console);
+
+//Calculando tu generación en base a tu edad
+let anioNacimiento;
+let asignaGeneracion=(anioNacimiento)=>{
+
+    switch(true){
+        case(anioNacimiento<1968):
+        return "Baby Boomer";
+
+        case(anioNacimiento>1968 && anioNacimiento<=1980):
+        return "Generación X";
+
+        case(anioNacimiento>1980 && anioNacimiento<=1994):
+        return "Generación Y/Millennial";
+
+        case(anioNacimiento>1994 && anioNacimiento<=2009):
+        return "Generación Z/Centennial";
+
+        case(anioNacimiento>2009):
+        return "Genración Alfa";
+    }
+}
+
+console.log(`Dado que nací en el año 1999 soy de la generación ${asignaGeneracion(2003)}`)
